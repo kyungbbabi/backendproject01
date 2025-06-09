4fc0760c51a1799b2999342418a92751cbbdcc84
kyungbbabi <kyung981124@naver.com>
2025-06-09 11:18:59 +0900

    Initial commit

---
+++ src/main/java/com/example/backendproject01/Backendproject01Application.java
@@ -0,0 +1,13 @@
+package com.example.backendproject01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backendproject01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Backendproject01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendproject01/Backendproject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendproject01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backendproject01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
