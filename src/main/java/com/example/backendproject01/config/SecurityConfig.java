b34082b82e48f716508c0c9bf7ac87f896c4f871
kyungbbabi <kyung981124@naver.com>
2025-06-16 00:02:27 +0900

    index, login, join 전부 security에서 허용해줌

--- src/main/java/com/example/backendproject01/config/SecurityConfig.java
+++ src/main/java/com/example/backendproject01/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/join").permitAll()
+                .requestMatchers("/", "/join", "/login").permitAll()
         )
                 .csrf(csrf -> csrf.disable());
 
