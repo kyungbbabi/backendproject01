2684015019016c032de0d260828e2e5f7635da21
kyungbbabi <kyung981124@naver.com>
2025-06-16 00:01:22 +0900

    login request java 추가

---
+++ src/main/java/com/example/backendproject01/dto/LoginRequest.java
@@ -0,0 +1,18 @@
+package com.example.backendproject01.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "아이디를 입력하세요")
+    private String loginId;
+    @NotBlank(message = "비밀번호를 입력하세요")
+    private String password;
+
+}
