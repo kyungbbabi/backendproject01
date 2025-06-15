406fb75cc38ca6d52a6993e5385c4bcc0ce6258e
kyungbbabi <kyung981124@naver.com>
2025-06-16 00:02:56 +0900

    login 구현

--- src/main/java/com/example/backendproject01/controller/SessionLoginController.java
+++ src/main/java/com/example/backendproject01/controller/SessionLoginController.java
@@ -1,7 +1,11 @@
 package com.example.backendproject01.controller;
 
 import com.example.backendproject01.dto.JoinRequest;
+import com.example.backendproject01.dto.LoginRequest;
+import com.example.backendproject01.entity.User;
 import com.example.backendproject01.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -30,7 +34,7 @@ public class SessionLoginController {
     }
 
     @PostMapping("/join")
-    public String join(JoinRequest joinRequest, BindingResult bindingResult) {
+    public String join(JoinRequest joinRequest, BindingResult bindingResult, Model model) {
 
         if (userService.checkLoginIdDuplicate(joinRequest.getLoginId())) {
             bindingResult.addError(new FieldError("joinRequest", "loginId", "로그인 아이디가 중복됩니다."));
@@ -39,12 +43,60 @@ public class SessionLoginController {
             bindingResult.addError(new FieldError("joinRequest", "passwordCheck", "비밀번호가 일치하지 않습니다."));
         }
 
+        // validation 에러가 있으면 다시 폼으로
         if(bindingResult.hasErrors()) {
+            model.addAttribute("siteName", "Backend Project 01");
+            model.addAttribute("loginType", "session-login");
             return "join";
         }
 
-        // validation 에러가 있으면 다시 폼으로
         userService.join(joinRequest);
-        return "redirect:/session-login";
+        return "/login";
     }
+
+    @GetMapping("/login")
+    public String loginPage(Model model) {
+
+        model.addAttribute("siteName", "Backend Project 01");
+        model.addAttribute("loginType", "session-login");
+        model.addAttribute("loginRequest", new LoginRequest());
+
+        return "login";
+
+    }
+
+    @PostMapping("/login")
+    public String login(LoginRequest loginRequest, HttpServletRequest httpServletRequest){
+        try {
+            // 1️⃣ 로그인 검증 및 사용자 정보 가져오기, UserService에서 아이디/비밀번호 확인 후 User 객체 반환
+            User user = userService.login(loginRequest);
+
+            // 세션에 로그인 정보 저장, 이제 다른 페이지에서도 session.getAttribute("user")로 접근 가능
+            HttpSession session = httpServletRequest.getSession();
+            session.setAttribute("user", user);
+
+            return "redirect:/";
+
+        } catch (Exception e) {
+            return "/login";
+        }
+
+    }
+
+    @GetMapping("/")
+    public String indexPage(Model model, HttpServletRequest httpServletRequest) {
+
+        HttpSession session = httpServletRequest.getSession();
+        if (session != null) {
+            User user = (User) session.getAttribute("user");
+            if (user != null) {
+                model.addAttribute("loginUser", user);
+                model.addAttribute("siteName", "Backend Project 01");
+
+                return "index";
+            }
+        }
+        return "redirect:/login";
+    }
+
 }
--- src/main/java/com/example/backendproject01/dto/JoinRequest.java
+++ src/main/java/com/example/backendproject01/dto/JoinRequest.java
@@ -16,7 +16,11 @@ public class JoinRequest {
 
     @NotBlank(message = "비밀번호가 비어있습니다.")
     private String password;
+
+    @NotBlank(message = "비밀번호 확인은 필수입니다.")
     private String passwordCheck;
+
+    @NotBlank(message = "이름은 필수입니다")
     private String name;
 
     // DTO를 Entity로 변환하는 메서드
