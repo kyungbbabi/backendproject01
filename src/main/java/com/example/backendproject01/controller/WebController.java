00ef1f1b68775d570c3fdfc9144f795fccbd3658
kyungbbabi <kyung981124@naver.com>
2025-06-23 09:47:30 +0900

    jwt 일때의 url주소 mapping

---
+++ src/main/java/com/example/backendproject01/controller/WebController.java
@@ -0,0 +1,32 @@
+package com.example.backendproject01.controller;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Profile("jwt")
+public class WebController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+    @GetMapping("/memo")
+    public String memo() {
+        return "memo";
+    }
+
+}
